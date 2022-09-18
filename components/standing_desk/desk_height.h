89e66f4dc49e1073a5b91b95e9e8fe1fd07ca2c0
Patrick Oyarzun <patrick.oyarzun@grafana.com>
2022-09-18 16:13:56 -0500

    First attempt at external component

--- components/standing_desk/desk_height.h
+++ components/standing_desk/desk_height.h
@@ -1,8 +1,13 @@
 #include "esphome.h"
 
+namespace esphome {
+namespace timotion {
+
 class DeskHeight : public Component, public UARTDevice, public Sensor {
  public:
   DeskHeight(UARTComponent *parent) : UARTDevice(parent) {}
+
+  char packet_header[3];
 
   void setup() override {
     // nothing to do here
@@ -15,15 +20,18 @@ class DeskHeight : public Component, public UARTDevice, public Sensor {
     // Anything else (16 bytes) is a status message
 
     // Read 3 bytes
-    char packet_header[3];
-    packet_header[0] = read();
-    packet_header[1] = read();
+    packet_header[0] = packet_header[1];
+    packet_header[1] = packet_header[2];
     packet_header[2] = read();
 
     // Check if it's a height packet
     if (packet_header[0] == 0x99 && 
         packet_header[1] == 0x40 && 
         packet_header[2] == 0x01) {
+
+        packet_header[0] = 0;
+        packet_header[1] = 0;
+        packet_header[2] = 0;
 
         // Read 2 bytes
         char packet_height[2];
@@ -45,3 +53,6 @@ class DeskHeight : public Component, public UARTDevice, public Sensor {
     }
   }
 };
+
+} // namespace timotion
+} // namespace esphome
